901bb02dca04a0953212f9e6a7c3bbca1b9e1b04
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-02 13:11:51 -0600

    3.11 Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -16,7 +16,7 @@ const ControlButtonElem = styled.div`
   ${(props) =>
     props.active &&
     css`
-      text-shadow: 0px 0px 60px #03ff03;
+      text-shadow: 0px 0px 20px #03ff03;
     `}
 `;
 
