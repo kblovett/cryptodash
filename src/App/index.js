0e8a6a5305d1242fed29f9ccaf98fec32059996f
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-02 11:55:18 -0600

    3.10 AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -3,10 +3,12 @@ import './App.css';
 
 import AppLayout from './AppLayout';
 import WelcomeMessage from './WelcomeMessage';
+import AppBar from './AppBar';
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage />
     </AppLayout>
   );
